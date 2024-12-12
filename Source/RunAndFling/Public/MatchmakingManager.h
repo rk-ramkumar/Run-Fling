345cac308a1f71527221473a7eaa85d47bc21f0e
ram <rramkumar1306@gmail.com>
2024-12-12 23:31:51 +0530

    Introduce match making manager object.

---
+++ Source/RunAndFling/Public/MatchmakingManager.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "GameFramework/PlayerController.h"
+#include "MatchmakingManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RUNANDFLING_API UMatchmakingManager : public UObject
+{
+	GENERATED_BODY()
+	
+};
